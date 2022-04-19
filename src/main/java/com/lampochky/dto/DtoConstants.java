34b80cd43881181e704190a1321106944af98215
FailureWitness <evgeny@vasyliev.com>
2022-04-19 20:28:32 +0300

    Change time format to 0-23 hours per day

--- src/main/java/com/lampochky/dto/DtoConstants.java
+++ src/main/java/com/lampochky/dto/DtoConstants.java
@@ -2,6 +2,6 @@ package com.lampochky.dto;
 
 public class DtoConstants {
     public static final String dateFormat = "dd-MM-yyyy";
-    public static final String timeFormat = "hh:mm:ss.SSS";
+    public static final String timeFormat = "HH:mm:ss.SSS";
     public static final String dateTimeFormat = dateFormat + " " + timeFormat;
 }
